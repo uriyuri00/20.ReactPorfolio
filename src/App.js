736127db2c9c5b00e0495e25d3b63062dd0ec3ae
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-04-08 23:11:25 -0700

    creat menu

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 // import logo from './logo.svg';
-import {AboutMe} from './component/AboutMe';
-import {Contact} from './component/Contact';
-import {Footer} from './component/Footer';
-import {Header} from "./component/Header";
-import {Navigation} from "./component/Navigation";
-import {Project} from "./component/Project"
-import {Resume} from "./component/Resume"
-import {Skills} from "./component/Skills";
-
+import React, { useState } from "react";
+import { AboutMe } from "./component/AboutMe";
+import { Contact } from "./component/Contact";
+import { Footer } from "./component/Footer";
+import { Header } from "./component/Header";
+import { Navigation } from "./component/Navigation";
+import { Project } from "./component/Project";
+import { Resume } from "./component/Resume";
+import { Skills } from "./component/Skills";
+import "./component/Style/App.css";
 
 const App = () => {
+  let [pageName, setPageName] = useState("AboutMe");
+
   return (
-    <div className='App'>
-      <Navigation />
-      <header className="header">
+    <div className="App">
+      <Navigation setPageName={setPageName} />
+      <header className="header" id="site-header">
         <Header />
-        <AboutMe />
       </header>
-      <main>
-        <Skills/>
-        <Project/>
-        <Resume />
+      <main id="site-main">
+        
+        {pageName==="AboutMe"?<AboutMe/>:""}
+        {pageName==="Skills"?<Skills/>:""}
+        {pageName==="Project"?<Project/>:""}
+        {pageName==="Resume"?<Resume/>:""}
+        {pageName==="Contact"?<Contact/>:""}
+
       </main>
       <footer>
-        <Footer/>
-        <Contact/>
-      
+        <Footer />
+        <Contact />
       </footer>
     </div>
   );
-}
-export default App
+};
+export default App;
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Style/Navigation.css";
 
-export const Navigation = () => {
+export const Navigation = (props) => {
+  let setPageName = props.setPageName;
+  // let {stagePageName} = props;
+
+  // Another way to have navigation links to change the page is:
+  // React-Router-Dom
+
+  // This way is called Conditional Rendering using useState and Props
+  
   return (
-    <div>Navigation</div>
-  )
-}
-
+    <nav id="navigation">
+      <ul>
+        <li>
+          <a onClick={()=>{ setPageName("AboutMe") }}>About Me</a></li>
+        <li>
+          <a onClick={()=>{ setPageName("Skills") }}>Skills</a></li>
+        <li>
+          <a onClick={()=>{ setPageName("Project") }}>Projects</a></li>
+        <li>
+          <a onClick={()=>{ setPageName("Resume") }}>Resume</a></li>
+        <li>
+          <a onClick={()=>{ setPageName("Contact") }}>Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
